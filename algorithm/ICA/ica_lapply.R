4ca92d7cf9b737f393a1af7521a35b5cd8733223
Haichuan Wang <hwang154@illinois.edu>
2015-03-02 15:57:50 -0600

    Refactoring ICA implementtion, add fastICA solution

--- algorithm/ICA/ica_lapply.R
+++ algorithm/ICA/ica_lapply.R
@@ -1,41 +1,18 @@
-# ica analysis
+# ICA Analysis - lapply based algorithm
 # 
 # Author: Haichuan Wang
 #
 # The code is based on FastICA R package http://cran.r-project.org/web/packages/fastICA/
 # Un-mixing n mixed independent uniforms
 ###############################################################################
-app.name <- "ICA"
+app.name <- "ICA_lapply"
+source('setup_ica.R')
 
-
-setup <- function(args=c('1000000', '2', '25')) {
-    n<-as.integer(args[1])
-    if(is.na(n)){ n <- 1000000L }
-    
-    nvar <-as.integer(args[2])
-    if(is.na(nvar)){ nvar <- 2L }
-    
-    
-    niter<-as.integer(args[3])
-    if(is.na(niter)){ niter <- 25L }
-    
-    cat('[INFO][', app.name, '] n=', n, ', nvar=', nvar, ', niter=', niter, '\n', sep='')
-    
-    #generate pre-centered data. Note the data shape is n x nvar
-    S <- matrix(runif(n*nvar), nrow=n, ncol=nvar)
-    A <- matrix(c(1, 1, -1, 3), 2, 2)
-    X <- scale(S %*% A, scale = FALSE) #pre-centering
-    X <- lapply(1:n, function(i){X[i,]})
-    data <- list(X = X, A = A, nvar=nvar, niter = niter)
-    
-    return(data)
-}
-
-run <- function(data) {
-    X <- data$X
-    A <- data$A
-    nvar <- data$nvar
-    niter <- data$niter
+run <- function(dataset) {
+    X <- dataset$X
+    A <- dataset$A
+    nvar <- dataset$nvar
+    niter <- dataset$niter
     n <- length(X) #num of samples
 
     message("Whitening")
@@ -85,8 +62,6 @@ run <- function(data) {
     resA <- t(w) %*% solve(w %*% t(w))
     print(resA)
 }
-
-
 
 if (!exists('harness_argc')) {
     data <- setup(commandArgs(TRUE))
