ee5c4c9bf66c58a400c067ddcf643779e389d1bb
Haichuan Wang <hwang154@illinois.edu>
2015-03-02 22:40:30 -0600

    Refine NN and k-NN

--- algorithm/k-NN/k-NN_lapply_mtrans_level1.R
+++ algorithm/k-NN/k-NN_lapply_mtrans_level1.R
@@ -1,53 +1,18 @@
-# Nearest Neighbor
-# 
-# Input 3-dim points, 10 categories, 'C1' to 'C10'.
-#   Traing: 10K. 
-#   Testing: 10K
+# k Nearest Neighbor - lapply based implementation with outer level manual vecapply
 # 
 # Author: Haichuan Wang
 ###############################################################################
+app.name <- "k-NN_lapply_mtrans_level1"
+source('setup_k-NN.R')
 library(vecapply)
-setup <- function(args=c('10000', '10000', '10', '5')) {
-    train_n<-as.integer(args[1])
-    if(is.na(train_n)){ train_n <- 10000L }
-    
-    test_n<-as.integer(args[2])
-    if(is.na(test_n)){ test_n <- 10000L }   
-    
-    clusters<-as.integer(args[3])
-    if(is.na(clusters)){ clusters <- 10L }
-    
-    k<-as.integer(args[4])
-    if(is.na(k)){ k <- 5L }    
-    
-    #generate training
-    mean_shift <- rep(0:(clusters-1), length.out = 3*train_n)
-    train_set <- matrix(rnorm(3*train_n, sd = clusters/2) + mean_shift, ncol=3)
-    list_train_set <- lapply(1:train_n, function(i) {
-                                     label_str <-paste('C', as.character(mean_shift[i]), sep="")
-                                     list(val=train_set[i,], label=label_str)
-                                 })
-                         
-    test_set <- matrix(runif(3*test_n, min=-clusters, max=2*clusters-1), ncol=3)
-    list_test_set <- lapply(1:test_n, function(i) {
-                list(val=test_set[i,])
-            })
-    
-    data <-list(train_set=list_train_set, 
-            test_set=list_test_set,
-            clusters=clusters,
-            k=k)
-    return(data)
-}
 
-run <- function(data) {
-    list_train<-data$train_set
+run <- function(dataset) {
+    list_train<-dataset$train_set
     train_n <- length(list_train)
-    list_test<-data$test_set
+    list_test<-dataset$test_set
     test_n <- length(list_test)
-    clusters<- data$clusters
-    k <- data$k
-    cat('k-NN: k =', k,', Category =', clusters, ', Train =', train_n, ', Test =', test_n, '\n')
+    clusters<- dataset$clusters
+    k <- dataset$k
     
     #outer loop, map function for each test
     
